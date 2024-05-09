6d0a6f91361b43b5e4e393184c35a22c7cd6deb0
Julio Bacoli <julio-microlins@hotmail.com>
2024-05-09 00:05:51 -0300

    Melhorando legibilidade

--- LerCSV/Program.cs
+++ LerCSV/Program.cs
@@ -23,17 +23,14 @@ public class Program
         if(!File.Exists(filePath))
             File.Create(filePath).Close();
 
-        using (var reader = new StreamReader(filePath))
+        using var reader = new StreamReader(filePath);
+        using (var csv = new CsvReader(reader, config))
         {
-            using (var csv = new CsvReader(reader, config))
-            {
-                csv.Context.TypeConverterOptionsCache.GetOptions<DateTime?>().NullValues.Add("NULL");
-                var atendimentos = csv.GetRecords<Atendimento>().ToList();
+            csv.Context.TypeConverterOptionsCache.GetOptions<DateTime?>().NullValues.Add("NULL");
+            var atendimentos = csv.GetRecords<Atendimento>().ToList();
 
-                foreach (var atendimento in atendimentos)
-                    Console.WriteLine($"Paciente: {atendimento.NomePaciente}, Dt Nascimento: {atendimento.DataNascimento} ,Clinica: {atendimento.NomeClinica}");
-            }
-
+            foreach (var atendimento in atendimentos)
+                Console.WriteLine($"Paciente: {atendimento.NomePaciente}, Dt Nascimento: {atendimento.DataNascimento} ,Clinica: {atendimento.NomeClinica}");
         }
 
     }
